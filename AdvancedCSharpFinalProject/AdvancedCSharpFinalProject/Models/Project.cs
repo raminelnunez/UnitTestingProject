c680bf9b525f8d178273d07e0c6ce65e0cbabf8d
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-04-02 16:48:55 -0500

    add properties AssignedBudget and ActualBudget. also add a method CalculateActualBudget in Project class

--- AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/Project.cs
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/Project.cs
@@ -7,10 +7,16 @@
         public Priority Priority { get; set; }
         public float CompletionPercentage { get; set; }
         public DateTime Deadline { get; set; }
+        public double AssignedBudget { get; set; }
+        public double ActualBudget { get; set; }
         public ProjectManager ProjectManager { get; set; }
         public string ProjectManagerId { get; set; }
         public List<ProjectTask> Tasks { get; set; }
         public List<Developer> Developers { get; set; }
+        public void CalculateActualBudget()
+        {
+            Developers.ForEach(developer => ActualBudget += developer.DailySalary);
+        }
     }
 }
 
