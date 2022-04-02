233c3640a004e09e48e7794b0b5f333369a8a6e1
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-04-02 18:28:05 -0500

    remove ICollection<Note>

--- AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/ProjectTask.cs
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/ProjectTask.cs
@@ -10,7 +10,6 @@
         public Priority Priority { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public ICollection<Note> Notes { get; set; }
         public DateTime Deadline { get; set; }
 
         public ProjectTask(Project project, Developer developer, string title, string description, DateTime deadline, Priority priority)
@@ -23,7 +22,6 @@
             Description = description;
             Deadline = deadline;
             Priority = priority;
-            Notes = new HashSet<Note>();
         }
 
         public ProjectTask()
