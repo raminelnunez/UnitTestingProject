3177bf272f160a7a5eac5fec74c66f87500f0555
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-04-02 18:28:52 -0500

    map relationship of ProjectManager, Project, Developer, ProjectTask

--- AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Data/ApplicationDbContext.cs
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Data/ApplicationDbContext.cs
@@ -13,8 +13,38 @@ namespace AdvancedCSharpFinalProject.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            //set primary key
+            builder.Entity<ProjectManager>().HasKey(projectManager => projectManager.Id);
+            builder.Entity<Project>().HasKey(project => project.Id);
+            builder.Entity<Developer>().HasKey(developer => developer.Id);
+            builder.Entity<ProjectTask>().HasKey(projectTask => projectTask.Id);
+            builder.Entity<Note>().HasKey(note => note.Id);
+
+            //ProjectManager to Project (One To Many)
+            builder.Entity<ProjectManager>()
+                .HasMany(projectManager => projectManager.Projects)
+                .WithOne(project => project.ProjectManager)
+                .HasForeignKey(project => project.ProjectManagerId);
+
+            //Project to Developer (Many To Many)
+            //breakTable: ProjectTask
+
+            //for Developer to ProjectTask
+            builder.Entity<ProjectTask>()
+                .HasOne(projectTask => projectTask.Developer)
+                .WithMany(developer => developer.ProjectTasks)
+                .HasForeignKey(projectTask => projectTask.DeveloperId);
+
+            //for Project to ProjectTask
+            builder.Entity<ProjectTask>()
+                .HasOne(projectTask => projectTask.Project)
+                .WithMany(project => project.ProjectTasks)
+                .HasForeignKey(projectTask => projectTask.ProjectId);
         }
         public DbSet<ProjectManager> ProjectManager { get; set; }
         public DbSet<Developer> Developer { get; set; }
+        public DbSet<Project> Project { get; set; }
+        public DbSet<ProjectTask> ProjectTask { get; set; }
     }
 }
