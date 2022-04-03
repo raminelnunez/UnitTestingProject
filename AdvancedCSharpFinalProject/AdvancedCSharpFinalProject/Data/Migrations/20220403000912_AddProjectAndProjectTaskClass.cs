e90f257e8d68e843ab638a1ddd08e37d338b3da4
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-04-02 19:11:13 -0500

    add migration to add Project and ProjectTask tables to database

---
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Data/Migrations/20220403000912_AddProjectAndProjectTaskClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AdvancedCSharpFinalProject.Data.Migrations
+{
+    public partial class AddProjectAndProjectTaskClass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    IsCompleted = table.Column<bool>(type: "bit", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    CompletionPercentage = table.Column<float>(type: "real", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    AssignedBudget = table.Column<double>(type: "float", nullable: false),
+                    ActualBudget = table.Column<double>(type: "float", nullable: false),
+                    ProjectManagerId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Project_AspNetUsers_ProjectManagerId",
+                        column: x => x.ProjectManagerId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectTask",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectId = table.Column<int>(type: "int", nullable: false),
+                    DeveloperId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Priority = table.Column<int>(type: "int", nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Deadline = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectTask", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProjectTask_AspNetUsers_DeveloperId",
+                        column: x => x.DeveloperId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectTask_Project_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Project",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_ProjectManagerId",
+                table: "Project",
+                column: "ProjectManagerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTask_DeveloperId",
+                table: "ProjectTask",
+                column: "DeveloperId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectTask_ProjectId",
+                table: "ProjectTask",
+                column: "ProjectId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectTask");
+
+            migrationBuilder.DropTable(
+                name: "Project");
+        }
+    }
+}
