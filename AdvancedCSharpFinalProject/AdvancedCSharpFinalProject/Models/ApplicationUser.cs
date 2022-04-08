8bd9a87f31c40b59b4d95e91acf625c3eeda7a8a
MuhammadMusab1 <yaqoobmusab03@gmail.com>
2022-04-08 00:10:46 -0500

    add IsDeveloper and IsProjectManager to ApplicationUser

--- AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/ApplicationUser.cs
+++ AdvancedCSharpFinalProject/AdvancedCSharpFinalProject/Models/ApplicationUser.cs
@@ -7,6 +7,8 @@ namespace AdvancedCSharpFinalProject.Models
         public double DailySalary { get; set; }
         public ICollection<ProjectTask> ProjectTasks { get; set; }
         public ICollection<Project> Projects { get; set; }
+        public bool IsDeveloper { get; set; }
+        public bool IsProjectManager { get; set; }
         public ApplicationUser()
         {
             Projects = new HashSet<Project>();
