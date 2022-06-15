7bdd6bd27f15aa50cb672e637a7b106d91886b1e
raminelnunez <Raminelnc23@gmail.com>
2022-06-15 10:42:28 -0500

    ....

---
+++ AdvancedCSharpFinalProject/UnitTestingProjectUnitTest/Notification_UnitTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AdvancedCSharpFinalProject.Data.BLL;
+using AdvancedCSharpFinalProject.Data.DAL;
+using AdvancedCSharpFinalProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestingProjectUnitTest
+{
+    [TestClass]
+    public class Notification_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ AdvancedCSharpFinalProject/UnitTestingProjectUnitTest/Project_UnitTest.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using AdvancedCSharpFinalProject.Data.BLL;
+using AdvancedCSharpFinalProject.Data.DAL;
+using AdvancedCSharpFinalProject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestingProjectUnitTest
+{
+    [TestClass]
+    public class Project_UnitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
